da5d56c2909318cf4f8c492dcb7564f86391fd96
Victor-Algo <victor.algo.contact@gmail.com>
2023-03-22 21:59:44 +0100

    add dataframe_info and change config

--- tests/src/config/test_table_config.py
+++ tests/src/config/test_table_config.py
@@ -5,18 +5,57 @@ from src.config.table_config import read_table_config, validate_config
 
 class TestTableConfig(unittest.TestCase):
 
-    def test_valid_config_raises_error_when_field_is_unknown(self):
-        config = {"unknown": []}
+    def test_valid_config_raises_error_when_dataframe_info_is_missing(self):
+        config = {}
         with self.assertRaises(ValueError):
             validate_config(config)
 
-    def test_valid_config_raises_error_when_field_is_not_list(self):
-        config = {"uniqueness": ""}
+    def test_valid_config_raises_error_when_dataframe_info_schema_is_invalid(self):
+        config = {
+            "dataframe_info": {
+                "env": "prod",
+                "source": "postgresql"
+            }
+        }
+        with self.assertRaises(ValidationError):
+            validate_config(config)
+
+    def test_valid_config_raises_error_when_constraint_field_is_unknown(self):
+        config = {
+            "dataframe_info": {
+                "env": "prod",
+                "source": "postgresql",
+                "schema": "website",
+                "table": "user"
+            },
+            "unknown": []
+        }
         with self.assertRaises(ValueError):
             validate_config(config)
 
-    def test_valid_config_raises_error_when_field_schema_is_invalid(self):
-        config = {"uniqueness": [{}]}
+    def test_valid_config_raises_error_when_constraint_field_is_not_list(self):
+        config = {
+            "dataframe_info": {
+                "env": "prod",
+                "source": "postgresql",
+                "schema": "website",
+                "table": "user"
+            },
+            "uniqueness": ""
+        }
+        with self.assertRaises(ValueError):
+            validate_config(config)
+
+    def test_valid_config_raises_error_when_constraint_schema_is_invalid(self):
+        config = {
+            "dataframe_info": {
+                "env": "prod",
+                "source": "postgresql",
+                "schema": "website",
+                "table": "user"
+            },
+            "uniqueness": [{}]
+        }
         with self.assertRaises(ValidationError):
             validate_config(config)
 
@@ -24,16 +63,22 @@ class TestTableConfig(unittest.TestCase):
         config = read_table_config("tests/resources/test_table_config.json")
         assert config == \
                {
+                   "dataframe_info": {
+                       "env": "prod",
+                       "source": "postgresql",
+                       "schema": "website",
+                       "table": "user"
+                   },
                    "uniqueness": [
                        {
-                           "name": "UniquePerson",
-                           "criticality": 3,
+                           "name": "UniquenessPerson",
+                           "criticality": 2,
                            "condition": ["first_name", "last_name"]
                        },
                        {
-                           "name": "UniqueCompany",
-                           "criticality": 1,
-                           "condition": ["siren"]
+                           "name": "UniquenessId",
+                           "criticality": 3,
+                           "condition": ["id"]
                        }
                    ],
                    "completeness": [
@@ -51,14 +96,9 @@ class TestTableConfig(unittest.TestCase):
                    ],
                    "accuracy": [
                        {
-                           "name": "AccuracyPrice",
-                           "criticality": 3,
-                           "condition": "price_excl_tax < 0.0 OR price_excl_tax < price_incl_tax"
-                       },
-                       {
-                           "name": "AccuracyProduct",
-                           "criticality": 1,
-                           "condition": "name LIKE ' %'"
+                           "name": "AccuracyAge",
+                           "criticality": 2,
+                           "condition": "age < 0"
                        }
                    ],
                    "freshness": [
@@ -72,9 +112,15 @@ class TestTableConfig(unittest.TestCase):
                    "integrity": [
                        {
                            "name": "IntegrityCustomerAddress",
-                           "criticality": 2,
+                           "criticality": 1,
                            "left_keys": ["id"],
-                           "right_keys": ["id"]
+                           "right_keys": ["id_customer"]
+                       },
+                       {
+                           "name": "IntegrityCustomerAccount",
+                           "criticality": 2,
+                           "left_keys": ["id", "last_name"],
+                           "right_keys": ["id_customer", "last_name"]
                        }
                    ]
                }
